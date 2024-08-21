b7ca56b13df58337b07623f05d3940224ce0427b
wangfeng <wangfengxy@sina.cn>
2024-08-21 15:26:27 +0800

    约束avo生成plan9汇编代码的关键字

---
+++ labs/asm/avx2/arch.go
@@ -0,0 +1,15 @@
+package avx2
+
+const (
+	Func_prefix_float32x8 = "f32x8"
+	Func_prefix_float64x4 = "f64x4"
+	Func_prefix_boolx8    = "b1x8"
+	Func_prefix_boolx32   = "b8x32"
+	Op_add                = "add"
+	Op_sub                = "sub"
+	Op_mul                = "mul"
+	Op_div                = "div"
+	Op_and                = "and"
+	Op_or                 = "or"
+	Op_xor                = "xor"
+)
