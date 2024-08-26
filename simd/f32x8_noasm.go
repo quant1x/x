b49452981e3a486112907b76b8b056d46ed2efd3
wangfeng <wangfengxy@sina.cn>
2024-08-26 13:21:48 +0800

    新增f32x8 add函数内不申请内存的

--- simd/f32x8_noasm.go
+++ simd/f32x8_noasm.go
@@ -8,3 +8,14 @@ func noasm_f32x8_add(x, y []float32) []float32 {
 	}
 	return r
 }
+
+func noasm_f32x8_add_v1(x, y, r []float32) int {
+	length := len(x)
+	n := length
+	step := 1
+	for i := 0; i < length; i += step {
+		r[i] = x[i] + y[i]
+		n -= step
+	}
+	return n
+}
