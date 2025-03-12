84ab9a537707b14211058e40464593d46cca1448
wangfeng <wangfengxy@sina.cn>
2025-03-13 04:45:37 +0800

    补充函数注释

--- std/mem/size.go
+++ std/mem/size.go
@@ -5,11 +5,13 @@ import (
 	"unsafe"
 )
 
+// 使用反射判断泛型类型的尺寸
 func v1TypeSize[T any]() uintptr {
 	typ := reflect.TypeOf((*T)(nil)).Elem()
 	return typ.Size()
 }
 
+// TypeSize 返回泛型类型的尺寸
 func TypeSize[T any]() uintptr {
 	var e T
 	return unsafe.Sizeof(e)
