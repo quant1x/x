c51bc45cf75eb4bf948c82de8600e3b1bb30d7ba
wangfeng <wangfengxy@sina.cn>
2025-03-14 17:03:04 +0800

    将该项目打包成一个控制台简单可以使用的工具, 并计划支持跨平台的守护进程

--- go.mod
+++ go.mod
@@ -3,12 +3,19 @@ module github.com/quant1x/x
 go 1.24.0
 
 require (
+	github.com/kardianos/service v1.2.2
 	github.com/mmcloughlin/avo v0.6.0
+	github.com/panjf2000/gnet/v2 v2.7.2
 	golang.org/x/sys v0.31.0
 )
 
 require (
+	github.com/panjf2000/ants/v2 v2.11.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/mod v0.24.0 // indirect
 	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/tools v0.31.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 )
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"github.com/kardianos/service"
+	"log"
+	"os"
+)
+
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	go p.run()
+	return nil
+}
+
+func (p *program) run() {
+	// 服务逻辑代码...
+}
+
+func (p *program) Stop(s service.Service) error {
+	return nil
+}
+
+func main() {
+	svcConfig := &service.Config{
+		Name:        "quant1x-x",
+		DisplayName: "Quant1X-Lab X",
+		Description: "Quant1X的实验工程",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(os.Args) > 1 {
+		// 支持命令行操作：install/uninstall/start/stop
+		err = service.Control(s, os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	err = s.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
