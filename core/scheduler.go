1d62453ad560246db2e9947e66db60ffbb76b4fc
wangfeng <wangfengxy@sina.cn>
2025-03-16 06:58:01 +0800

    增加专用于内置功能的任务调度器

---
+++ core/scheduler.go
@@ -0,0 +1,53 @@
+package core
+
+import (
+	"fmt"
+	"github.com/go-co-op/gocron/v2"
+	"sync"
+	"time"
+)
+
+var (
+	//global_context, global_cancel = context.WithCancel(context.Background())
+	global_scheduler_cron     gocron.Scheduler
+	global_scheduler_once     sync.Once
+	global_scheduler_location = time.Local
+	//scheduler_done            = make(chan struct{})
+)
+
+// 初始化全局调度器
+func lazyInitScheduler() {
+	// 创建一个调度器
+	cron, err := gocron.NewScheduler(
+		gocron.WithLocation(global_scheduler_location),
+		gocron.WithLogger(logger),
+	)
+	if err != nil {
+		panic(err)
+	}
+	global_scheduler_cron = cron
+	global_scheduler_cron.Start()
+	_ = RegisterHook("scheduler", stopScheduler)
+}
+
+func stopScheduler() {
+	fmt.Println("stop scheduler-1")
+	err := global_scheduler_cron.Shutdown()
+	if err != nil {
+		logger.Error("x/cron: scheduler shutdown err:", err)
+	}
+	fmt.Println("stop scheduler-2")
+}
+
+func AddJob(spec string, cmd func()) error {
+	global_scheduler_once.Do(lazyInitScheduler)
+	jd := gocron.CronJob(spec, true)
+	task := gocron.NewTask(cmd)
+	// 添加一个job到全局调度器
+	job, err := global_scheduler_cron.NewJob(jd, task)
+	if err != nil {
+		return err
+	}
+	_ = job
+	return nil
+}
