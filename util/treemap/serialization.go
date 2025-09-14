62210d217e4c3e5bf8d45aa5ccb5d1c070c737e1
wangfeng <wangfengxy@sina.cn>
2025-09-14 13:36:24 +0800

    增补TreeMap

---
+++ util/treemap/serialization.go
@@ -0,0 +1,24 @@
+// Copyright (c) 2015, Emir Pasic. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package treemap
+
+import (
+	"github.com/quant1x/x/util/internal"
+)
+
+func assertSerializationImplementation() {
+	var _ internal.JSONSerializer = (*Map)(nil)
+	var _ internal.JSONDeserializer = (*Map)(nil)
+}
+
+// ToJSON outputs the JSON representation of the map.
+func (m *Map) ToJSON() ([]byte, error) {
+	return m.tree.ToJSON()
+}
+
+// FromJSON populates the map from the input JSON representation.
+func (m *Map) FromJSON(data []byte) error {
+	return m.tree.FromJSON(data)
+}
