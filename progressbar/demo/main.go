33e117b077cd4aaaeaeaba6af173802d681adaf1
wangfeng <wangfengxy@sina.cn>
2025-09-14 13:30:35 +0800

    增补进度条和tags工具包

---
+++ progressbar/demo/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"sync"
+	"time"
+
+	"github.com/quant1x/x/logger"
+
+	pgbar "github.com/quant1x/x/progressbar"
+)
+
+func v1ProgressBarTest() {
+	b := pgbar.NewBar(1, "1st", 20000)
+	//b2 := pgbar.NewBar(2, "2st", 10000)
+	//b3 := pgbar.NewBar(3, "3st", 30000)
+	//b4 := pgbar.NewBar(4, "4st", 4000)
+	b.SetSpeedSection(900, 100)
+	//b2.SetSpeedSection(900, 100)
+	//b3.SetSpeedSection(900, 100)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 20000; i++ {
+			b.Add(1)
+			time.Sleep(time.Second / 2000)
+		}
+	}()
+
+	//go func() {
+	//	defer wg.Done()
+	//	for i := 0; i < 10000; i++ {
+	//		b2.Add(1)
+	//		time.Sleep(time.Second / 1000)
+	//	}
+	//}()
+	//go func() {
+	//	defer wg.Done()
+	//	for i := 0; i < 30000; i++ {
+	//		b3.Add(1)
+	//		time.Sleep(time.Second / 3000)
+	//	}
+	//}()
+	//
+	//go func() {
+	//	defer wg.Done()
+	//	for i := 0; i < 4000; i++ {
+	//		b4.Add(1)
+	//		time.Sleep(time.Second / 300)
+	//	}
+	//}()
+	wg.Wait()
+}
+
+func v2ProgressBarTest() {
+	//fmt.Printf("\n\n\n")
+	b := pgbar.NewBar(1, "1st", 20000)
+	b2 := pgbar.NewBar(2, "2st", 10000)
+	b3 := pgbar.NewBar(3, "3st", 30000)
+	b4 := pgbar.NewBar(4, "4st", 4000)
+	b.SetSpeedSection(900, 100)
+	b2.SetSpeedSection(900, 100)
+	b3.SetSpeedSection(900, 100)
+
+	var wg sync.WaitGroup
+	wg.Add(4)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 20000; i++ {
+			b.Add(1)
+			time.Sleep(time.Second / 2000)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 10000; i++ {
+			b2.Add(1)
+			time.Sleep(time.Second / 1000)
+		}
+	}()
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 30000; i++ {
+			b3.Add(1)
+			time.Sleep(time.Second / 3000)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 4000; i++ {
+			b4.Add(1)
+			time.Sleep(time.Second / 300)
+		}
+	}()
+	wg.Wait()
+}
+
+func main() {
+	logger.InitLogger("/opt/logs/test", logger.INFO)
+	for i := 0; i < 100; i++ {
+		v2ProgressBarTest()
+		//fmt.Printf("\n\n\n\n\n====================================================================================================")
+	}
+}
---
+++ progressbar/print.go
@@ -0,0 +1,143 @@
+package progressbar
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/quant1x/x/api"
+)
+
+var (
+	mu           sync.Mutex
+	gSrcLine     = 0 //起点行
+	gCurrentLine = 0 //当前行
+	gMaxLine     = 0 //最大行
+)
+
+func Reset() {
+	mu.Lock()
+	defer mu.Unlock()
+	gSrcLine = 0
+	gCurrentLine = 0
+	gMaxLine = 0
+}
+
+func GetMaxLine() int {
+	return gMaxLine
+}
+
+func SetMaxLine(line int) {
+	mu.Lock()
+	defer mu.Unlock()
+	gMaxLine = line
+}
+
+func adjustLine(line int) int {
+	mu.Lock()
+	defer mu.Unlock()
+	old := gMaxLine
+	if line <= 0 {
+		gMaxLine++
+		line = gMaxLine
+	}
+	if line > gMaxLine {
+		gMaxLine = line
+	}
+	if old > 0 && gMaxLine > old {
+		fmt.Printf(strings.Repeat("\r\n", gMaxLine-old))
+	}
+	_ = old
+	return line
+}
+
+// 移动光标到指定的进度条的行号
+func barMove(line int) {
+	fmt.Printf("\033[%dA\033[%dB", gCurrentLine, line)
+	gCurrentLine = line
+}
+
+func barPrint(line int, args ...any) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	barMove(line)
+	var realArgs []any
+	realArgs = append(realArgs, "\r")
+	realArgs = append(realArgs, args...)
+	fmt.Print(realArgs...)
+	barMove(gMaxLine)
+}
+
+func barPrintf(line int, format string, args ...any) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	barMove(line)
+	fmt.Printf("\r"+format, args...)
+	barMove(gMaxLine)
+}
+
+func barPrintln(line int, args ...any) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	barMove(line)
+	var realArgs []any
+	realArgs = append(realArgs, "\r")
+	realArgs = append(realArgs, args...)
+	fmt.Print(realArgs...)
+	barMove(gMaxLine)
+}
+
+func Print(args ...any) {
+	mu.Lock()
+	lf := countLF("", args...)
+	if gMaxLine == 0 {
+		gMaxLine += lf + 1
+	} else {
+		gMaxLine += lf
+	}
+	mu.Unlock()
+
+	barPrint(gMaxLine, args...)
+}
+
+func Printf(format string, args ...any) {
+	mu.Lock()
+
+	lf := countLF(format, args...)
+	if gMaxLine == 0 {
+		gMaxLine += lf + 1
+	} else {
+		gMaxLine += lf
+	}
+	mu.Unlock()
+
+	barPrintf(gMaxLine, format, args...)
+}
+
+func Println(args ...any) {
+	mu.Lock()
+
+	lf := countLF("", args...)
+	lf++
+	if gMaxLine == 0 {
+		gMaxLine += lf + 1
+	} else {
+		gMaxLine += lf
+	}
+	mu.Unlock()
+
+	barPrintln(gMaxLine, args...)
+}
+
+func countLF(format string, args ...any) int {
+	var count int
+	count = strings.Count(format, "\n")
+	for _, arg := range args {
+		count += strings.Count(api.ToString(arg), "\n")
+	}
+
+	return count
+}
