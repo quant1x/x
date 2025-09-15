3780ae10d360367223b4343789abc97d6843790b
wangfeng <wangfengxy@sina.cn>
2025-09-15 15:31:22 +0800

    调整局部变量名

--- core/scheduler.go
+++ core/scheduler.go
@@ -9,9 +9,9 @@ import (
 
 var (
 	//global_context, global_cancel = context.WithCancel(context.Background())
-	global_scheduler_cron     gocron.Scheduler
-	global_scheduler_once     sync.Once
-	global_scheduler_location = time.Local
+	globalSchedulerCron     gocron.Scheduler
+	globalSchedulerOnce     sync.Once
+	globalSchedulerLocation = time.Local
 	//scheduler_done            = make(chan struct{})
 )
 
@@ -19,20 +19,20 @@ var (
 func lazyInitScheduler() {
 	// 创建一个调度器
 	cron, err := gocron.NewScheduler(
-		gocron.WithLocation(global_scheduler_location),
+		gocron.WithLocation(globalSchedulerLocation),
 		//gocron.WithLogger(logger),
 	)
 	if err != nil {
 		panic(err)
 	}
-	global_scheduler_cron = cron
-	global_scheduler_cron.Start()
+	globalSchedulerCron = cron
+	globalSchedulerCron.Start()
 	_ = RegisterHook("scheduler", stopScheduler)
 }
 
 func stopScheduler() {
 	//fmt.Println("stop scheduler-1")
-	err := global_scheduler_cron.Shutdown()
+	err := globalSchedulerCron.Shutdown()
 	if err != nil {
 		//logger.Error("x/cron: scheduler shutdown err:", err)
 	}
@@ -40,12 +40,12 @@ func stopScheduler() {
 }
 
 func AddJob(spec string, cmd func()) error {
-	global_scheduler_once.Do(lazyInitScheduler)
+	globalSchedulerOnce.Do(lazyInitScheduler)
 	jd := gocron.CronJob(spec, true)
 	task := gocron.NewTask(cmd)
 	// 添加一个job到全局调度器
 	// 如果作业已在运行，则 WithSingletonMode 可防止作业再次运行。 这对于不应重叠的作业很有用，并且偶尔 （但不一致）运行时间长于作业运行之间的间隔。
-	job, err := global_scheduler_cron.NewJob(jd, task, gocron.WithSingletonMode(gocron.LimitModeReschedule))
+	job, err := globalSchedulerCron.NewJob(jd, task, gocron.WithSingletonMode(gocron.LimitModeReschedule))
 	if err != nil {
 		return err
 	}
